ae394d0cbce08a599924f50cd914cc890bfb39ff
Rifath98-git <degrausher@gmail.com>
2023-05-06 11:16:26 +0000

    LeFramponneur

---
+++ somme.py
@@ -0,0 +1,9 @@
+#trouver la somme de deux nombres entiers 
+nbre1 = input("entrez le premier nombre : ")
+nbre2 = input("entrez le deuxième nombre : ")
+
+#additionner les deux premiers nombres
+s = int(nbre1) + int(nbre2)
+
+#afficher la somme
+print("la somme de {0} et {1} est {2}".format(nbre1, nbre2, s))
